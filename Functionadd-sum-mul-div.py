f72bee75ef3fa99aadcf946a1d3924c36f13d8af
mukulR-rep <61862380+mukulR-rep@users.noreply.github.com>
2021-11-14 20:21:39 +0530

    Add files via upload

---
+++ Functionadd-sum-mul-div.py
@@ -0,0 +1,13 @@
+#write a program to find addition of two numbersusing Function
+def add():
+    a=int(input("Enter Number:"));
+    b=int(input("Enter Number:"));
+    c=a+b
+    print("Addition=",c)
+    d=a-b
+    print("Substraction=",d)
+    e=a*b
+    print("Multiplication=",e)
+    f=a/b
+    print("Divide=",f)
+add()    

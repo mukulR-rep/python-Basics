f6dd792ccfe7d18c8b49bd4d6aa8811fa93424e8
mukulR-rep <61862380+mukulR-rep@users.noreply.github.com>
2021-11-18 00:34:10 +0530

    Add files via upload

---
+++ smallOf2num.py
@@ -0,0 +1,9 @@
+#Write a program to find smallest of given two numbers?
+a=int(input("Enter 1st Number"));
+b=int(input("Enter 2nd Number:"));
+if a<b:
+    print("Smallest one is:",a)
+elif a>b:
+    print("Smallest one is:",b)
+else:
+    print("Both are equal")

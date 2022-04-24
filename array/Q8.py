c15f0b00a2de41d77958aaafc708de82c5e7107f
mukulR-rep <61862380+mukulR-rep@users.noreply.github.com>
2022-04-23 18:00:24 -0700

    Add files via upload

---
+++ array/Q8.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program to convert an array to an array of machine values and return the bytes representation.
+'''
+from array import *
+print("Bytes to String: ")
+x = array('b', [119, 51, 114, 101,  115, 111, 117, 114])
+s = x.tobytes()
+print(s)
